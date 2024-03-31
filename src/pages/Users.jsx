bcd2ff60087f6d67635edff20a137c6ef7134695
M0staphaTaha <darsh.taha12382@gmail.com>
2024-03-31 15:49:23 +0200

    roles in  users

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,53 +1,129 @@
 import React, { useEffect, useState } from 'react';
 import NavBar from '../components/NavBar';
 import { Authurization } from '../api/Api';
-import { Link } from 'react-router-dom';
 
 const Users = () => {
     const [productData, setProductData] = useState(null);
-    // const [authorizationToken, setAuthorizationToken] = useState(''); // For authorization token
     const [isLoading, setIsLoading] = useState(false);
-
-   
+    const [isOpen, setIsOpen] = useState(false);
+    const [isLoading2, setIsLoading2] = useState(false);
+    const [productData2, setProductData2] = useState(null);
+    const [customerId, setIdcategory] = useState('');
+    const [role, setRole] = useState('');
+    const toggleNavbar = (index) => {
+        setIsOpen(isOpen === index ? null : index)
+    }
 
     useEffect(() => {
         const fetchData = async () => {
-            setIsLoading(true); // Set loading state to true
-
+            setIsLoading(true);
             try {
                 const response = await fetch(`https://api.vitaparapharma.com/api/v1/admin/customer/all`, {
                     headers: {
                         Authorization: `Bearer ${Authurization}`,
-                        Accept: 'application/json', // Assuming JSON response
-                        // 'Accept-Language': 'en' // Set language preference to English
+                        Accept: 'application/json', 
                     }
                 });
                 const data = await response.json();
                 console.log(data);
-                setProductData(data); // Update product data
+                setProductData(data); 
                 console.log(productData);
-                console.log('Authorization token:', Authurization);
+                // console.log('Authorization token:', Authurization);
             } catch (error) {
                 console.error('Error fetching data:', error);
-                // Handle errors gracefully (e.g., display an error message)
             } finally {
-                setIsLoading(false); // Set loading state to false
+                setIsLoading(false); 
             }
         };
 
         fetchData();
     }, []);
+
+    const handleCustomerRole = async () => { 
+        try {
+            const response = await fetch(`https://api.vitaparapharma.com/api/v1/super/admin/customer/role`, {
+                method: 'PUT',
+                headers: {
+                    Authorization: `Bearer ${Authurization}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    customerId,
+                    role,
+                }),
+            });
+            const data = await response.json();
+            if (response.ok) {
+                console.log(data);
+                console.log(Authurization);
+                alert('Role Updated successfully:)');
+                console.log('Role Updated successfully:)', data);
+            }
+            console.log(data);
+        } catch (error) {
+            console.error('Error submitting form:', error);
+        }
+    };
+
+    const handleEnable = async () => { 
+        try {
+            const response = await fetch(`https://api.vitaparapharma.com/api/v1/super/admin/customer/enable/${customerId}`, {
+                method: 'PUT',
+                headers: {
+                    Authorization: `Bearer ${Authurization}`,
+                    'Content-Type': 'application/json',
+                },
+                
+            });
+            const data = await response.json();
+            if (response.ok) {
+                console.log(data);
+                alert('Role Updated successfully:)');
+                console.log('Role Updated successfully:)', data);
+            }
+            console.log(data);
+        } catch (error) {
+            console.error('Error submitting form:', error);
+        }
+    };
+
+    const handleDisable = async () => { 
+        try {
+            const response = await fetch(`https://api.vitaparapharma.com/api/v1/super/admin/customer/disable/${customerId}`, {
+                method: 'PUT',
+                headers: {
+                    Authorization: `Bearer ${Authurization}`,
+                    'Content-Type': 'application/json',
+                },
+                
+            });
+            const data = await response.json();
+            if (response.ok) {
+                console.log(data);
+                alert('Role Updated successfully:)');
+                console.log('Role Updated successfully:)', data);
+            }
+            console.log(data);
+        } catch (error) {
+            console.error('Error submitting form:', error);
+        }
+    };
+    
+    console.log(role);
     return (
         <>
             <div>
                 <NavBar />
                 <div className='text-white grid justify-center items-center py-20 h-screen w-screen overflow-x-auto '>
-                    <div className="relative overflow-x-auto shadow-md sm:rounded-lg 2xl:translate-x-[120px] xl:translate-x-[200px] lg:translate-x-[300px]  translate-y-28 ">
+                    <div className="relative overflow-x-auto shadow-md sm:rounded-lg 2xl:translate-x-[120px] xl:translate-x-[200px] lg:translate-x-[300px]  translate-y-28 m-5">
                         <table className="w-[1600px] max-[1815px]:w-[1300px] text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 overflow-x-auto ">
                             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 overflow-x-auto">
                                 <tr>
                                     <th scope="col" className="px-6 py-3">
                                         Name
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Phone
                                     </th>
                                     <th scope="col" className="px-6 py-3">
                                         Position
@@ -56,52 +132,115 @@ const Users = () => {
                                         Status
                                     </th>
                                     <th scope="col" className="px-6 py-3">
+                                        Locked
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
                                         Action
                                     </th>
                                 </tr>
                             </thead>
                             {isLoading ? (
-                            <span>Loading customer details...</span>
-                        ) : productData && productData.data.products !== null ? (
-                            <tbody>
-                                {productData.data.customers.map((product) => (
-                                <tr key={product.customerId} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 overflow-x-auto">
-                                    <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                                        <img className="w-10 h-10 rounded-full" src="/images/man-light-skin-tone-beard.svg" alt="Jese image" />
-                                        <div className="ps-3">
-                                            <div className="text-base font-semibold">{product.firstName} {product.lastName}</div>
-                                            <div className="font-normal text-gray-300">{product.email}</div>
-                                        </div>
-                                    </th>
-                                    <td className="px-6 py-4">
-                                        {product.role}
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        <div className="flex items-center">
-                                            {
-                                                product.enabled ? (
-                                                    <div className='flex items-center'>
-                                                        <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Enabled
-                                                    </div>
-                                                    
-                                                    ):(
-                                                        
-                                                        <div className='flex items-center'>
-                                                            <div className="h-2.5 w-2.5 rounded-full bg-red-500 me-2"></div> Not Enabled
-                                                        </div>
-                                                )
-                                            }
-                                        </div>
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        <Link to='/userpreview' className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</Link>
-                                    </td>
-                                </tr>
-                                ))}
-                            </tbody>
-                        ):(
-                            <span>No product data found.</span>
-                        )}
+                                <span>Loading customer details...</span>
+                            ) : productData && productData.data.products !== null ? (
+                                <tbody>
+                                    {productData.data.customers.map((product, index) => (
+                                        <tr key={product.customerId} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 overflow-x-auto">
+                                            <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                                                <img className="w-10 h-10 rounded-full" src="/images/man-light-skin-tone-beard.svg" alt="Jese image" />
+                                                <div className="ps-3">
+                                                    <div className="text-base font-semibold">{product.firstName} {product.lastName}</div>
+                                                    <div className="font-normal text-gray-300">{product.email}</div>
+                                                </div>
+                                            </th>
+                                            <td className="px-6 py-4">
+                                                {product.phone}
+                                            </td>
+                                            <td className="px-6 py-4">
+                                                {product.role}
+                                            </td>
+                                            <td className="px-6 py-4">
+                                                <div className="flex items-center">
+                                                    {
+                                                        product.enabled ? (
+                                                            <div className='flex items-center'>
+                                                                <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Enabled
+                                                            </div>
+
+                                                        ) : (
+
+                                                            <div className='flex items-center'>
+                                                                <div className="h-2.5 w-2.5 rounded-full bg-red-500 me-2"></div> Disabled
+                                                            </div>
+                                                        )
+                                                    }
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4">
+                                                <div className="flex items-center">
+                                                    {
+                                                        !product.locked ? (
+                                                            <div className='flex items-center'>
+                                                                <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> No
+                                                            </div>
+
+                                                        ) : (
+
+                                                            <div className='flex items-center'>
+                                                                <div className="h-2.5 w-2.5 rounded-full bg-red-500 me-2"></div> Yes
+                                                            </div>
+                                                        )
+                                                    }
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4">
+                                                <div> {/**fetchData2(product.customerId); */}
+                                                    <button onClick={() => { toggleNavbar(index); setIdcategory(product.customerId); }} id="dropdownActionButton" data-dropdown-toggle="dropdownAction" className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
+                                                        <span className="sr-only">Action button</span>
+                                                        Set Role
+                                                        <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+                                                        </svg>
+                                                    </button>
+                                                    {
+
+                                                        isOpen === index && (
+
+                                                            <div id="dropdownAction" className="z-10 absolute bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                                                                <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownActionButton">
+                                                                    <li>
+                                                                        <button onClick={()=>{setRole("super_admin"); handleCustomerRole();}} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Super Admin</button>
+                                                                    </li>
+                                                                    <li>
+                                                                        <button onClick={()=>{setRole("admin");handleCustomerRole(); }} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Admin</button>
+                                                                    </li>
+                                                                    <li>
+                                                                        <button onClick={()=>{setRole("customer");handleCustomerRole(); }} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Customer</button>
+                                                                    </li>
+                                                                    <li>
+                                                                        <button onClick={()=>{setRole("content_creator"); handleCustomerRole();}} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Content Creator</button>
+                                                                    </li>
+                                                                    <li>
+                                                                        <button onClick={()=>{setRole("delivery_boy"); handleCustomerRole();}} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Delivery Boy</button>
+                                                                    </li>
+                                                                </ul>
+                                                                <div className="py-1">
+                                                                {!product.locked ?
+                                                                    <button onClick={()=>handleEnable()} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Lock</button>
+                                                                    :
+                                                                    <button onClick={()=>handleDisable()} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Open</button>
+
+                                                                }
+                                                                </div>
+                                                            </div>
+                                                        )}
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            ) : (
+                                <span>No product data found.</span>
+                            )}
                         </table>
                     </div>
                 </div>
